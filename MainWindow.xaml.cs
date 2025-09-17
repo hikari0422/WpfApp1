4d13cd8ae51d946a61eccddee113025430f72c2e
hikari <ben109092ben@gmail.com>
2025-09-17 11:57:12 +0800

    update

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            ansBox.Clear();
+
+            try
+            {
+                int n = Convert.ToInt32(input.Text);
+                for (int i = 1; i <= 9; i++)
+                {
+                    ansBox.Text += $"{i} x {n} = {i * n}\n";
+                }
+            }
+            catch (FormatException)
+            {
+                input.Clear();
+                MessageBox.Show("請輸入有效數字！");
+            }
+        }
+    }
+}
