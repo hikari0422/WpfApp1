654583f58f712d7975a2a89caa060b5fa545bcb5
hikari <ben109092ben@gmail.com>
2025-09-24 10:19:01 +0800

    upload OrderPlane

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            var TargetTextBox = sender as TextBox;
+            int amount;
+            bool success = int.TryParse(TargetTextBox.Text, out amount);
+            if (!success || amount <= 0)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
